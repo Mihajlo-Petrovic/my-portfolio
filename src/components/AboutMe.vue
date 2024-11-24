63a911650a45ace1d757ee73ecd5a46c8afc4191
Mihajlo-Petrovic <mihajlo308@gmail.com>
2024-11-25 00:56:41 +0100

    .

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -16,8 +16,8 @@
                 <h3 class="fw-bold mb-5">Skills</h3>
 
                 <p><span class="color-purple">Frontend:</span> &nbsp; HTML, CSS, JavaScript, Vue.js, Bootstrap</p>
-                <p><span class="color-purple">Backend:</span> &nbsp; PHP, Laravel, C#</p>
-                <p><span class="color-purple">Databases:</span> &nbsp; MySQL, MariaDB</p>
+                <p><span class="color-purple">Backend:</span> &nbsp; PHP, Laravel, C#, .NET</p>
+                <p><span class="color-purple">Databases:</span> &nbsp; MySQL, MariaDB, Microsoft SQL Server</p>
                 <p><span class="color-purple">Tools:</span> &nbsp; Postman, Git, Owasp ZAP</p>
             </section>
 
@@ -32,7 +32,7 @@
                     mechanics. These experiences sparked my interest in programming, and over the years, I’ve developed a range of web and desktop applications. 
                     From simple school apps and calculators to more complex projects like an anti-bullying blog site and an online music platform, I’ve always 
                     been eager to explore new technologies and refine my craft. The highlight of my development journey so far has been the creation of NovaKeys, 
-                    a digital marketplace that combines a variety of features, including localized payment options, multi-language support, referral systems, and 
+                    a digital marketplace that combines a variety of features, including localized payment options, multi-language support, referral system and 
                     more. With NovaKeys, I’ve gained significant experience working with complex backend systems, databases, and building user-centric platforms.
                     What excites me most about development is the ability to create meaningful solutions that make a real-world impact. I’m particularly 
                     passionate about building scalable and user-friendly applications, and I’m always eager to learn new technologies and improve my skills.
