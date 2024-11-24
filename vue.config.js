28b759536dd37f420831191cd80a41065009e070
Mihajlo-Petrovic <mihajlo308@gmail.com>
2024-11-24 09:58:32 +0100

    .

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/my-portfolio/',
   publicPath: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/'
 })
